cbf4e54ce0bd4e1a4b9841a8891ffe65c2c4c87d
Lyang Medeiros <lyang.mdrs@gmail.com>
2019-02-28 22:00:09 -0300

    Adicionei os comando para carrega a estratégia dos roboôs

--- simulador/iniciar.sh
+++ simulador/iniciar.sh
@@ -4,3 +4,6 @@
 #
 gnome-terminal -- ./VSS-Simulator/build/vss-simulator &
 gnome-terminal -- ./VSS-Viewer/build/vss-viewer &
+gnome-terminal -- ./VSS-SampleCpp/configure.sh &
+gnome-terminal -- ./VSS-SampleCpp/build/vss-sample &
+gnome-terminal -- ./VSS-SampleCpp/build/vss-sample -t blue &
