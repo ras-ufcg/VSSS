7e6768724b63ad5c6ab04251494cdb4862dafd4d
ras-ufcg <ras@ee.ufcg.edu.br>
2019-05-01 15:57:27 -0300

    Struc da posição da bola

--- simulador/VSS-SampleCpp/src/generic_player.hpp
+++ simulador/VSS-SampleCpp/src/generic_player.hpp
@@ -7,6 +7,13 @@
 #include <iostream>
 #include "utils.hpp"
 
+struct ball_pos{
+    float xb = 0;
+    float yb = 0;
+    float old_xb = 0;
+    float old_yb = 0;
+}bolinha;
+
 /**
  * @brief Guarda a lógica de funcionamento do jogador de camisa vermelha (goleiro)
  *
@@ -47,29 +54,25 @@ class GenericPlayer {
              * vxb, vyb - representação de um vetor unitário no sentido de variação de movimento da bola
              * xg, yg - coordenadas do goleiro
              * d1 e d2 limites estratégicos, 40 3 70 cm respec.
-             * ymax, ymin - limites dá área do goleiro 
+             * ymax, ymin - limites dá área do goleiro
              */
 
             vss::Robot robot = (this->_teamType == vss::TeamType::Blue) ? state.teamBlue[index] : state.teamYellow[index];
-            
-            // Obtenção de valores para as variáveis
-            
-            // Obtem as coordenadas da bola
-            float xb = state.ball.x;
-            float yb = state.ball.y;
 
-            // Vairáveis auxiliares para guardar posições antigas da bola
-            volatile float old_xb = 0;
-            volatile float old_yb = 0;
+            // Obtenção de valores para as variáveis
+
+            // Obtem as coordenadas da bola
+
+            bolinha.xb = state.ball.x;
+            bolinha.yb = state.ball.y;
 
             // Obtem o vetor variação de acordo com os últimos dois estados recebidos
-            float vxb = calcula_variacao_x(xb, old_xb);
-            float vyb = calcula_variacao_y(yb, old_yb);
-
+            float vxb = calcula_variacao_x(bolinha.xb, bolinha.old_xb);
+            float vyb = calcula_variacao_y(bolinha.yb, bolinha.old_yb);
             // Atualização dos valores antigos  o_0
-            old_xb = xb;
-            old_yb = yb;
-            
+
+            bolinha.old_xb = bolinha.xb;
+            bolinha.old_yb = bolinha.yb;
             // DEBUGG LINES
             printf("Variacao em X: %f \n", vxb);
             printf("Variacao em Y: %f \n", vyb);
@@ -79,12 +82,12 @@ class GenericPlayer {
             //float yg = robot.y;
 
             // Definição de constantes
-            
+
             // a constante d1, marca uma linha em y à 40 cm do gol do nosso time
             // nas coordenadas do campo da simulação, esse valor equivale á 50.77
             float d1 = 80.77;
 
-            // a constante d2 também demarca uma linha só que numa distância de 
+            // a constante d2 também demarca uma linha só que numa distância de
             // 70 cm do nosso gol. o que na simulação equivale à 88.53
             float d2 = 108.53;
 
@@ -97,34 +100,34 @@ class GenericPlayer {
 
             // Calcula a tinha de interceção entre a provável trajetória da bola e
             // a linha de ação do goleiro
-            float y_inters = yb - ((vyb * (xb - xg))/(vxb));
+            float y_inters = bolinha.yb - ((vyb * (bolinha.xb - xg))/(vxb));
 
             // Máquina de estados
 
             volatile char estado = 'a';
-            
+
             // Regras de transição
 
             if(estado == 'a')
-            {   
+            {
                 // Regra 1
-                estado = ((xb > d1) && (vxb < 0))? 'b' : 'a';
+                estado = ((bolinha.xb > d1) && (vxb < 0))? 'b' : 'a';
                 // Regra 2
-                estado = ((xb < d2) && (vxb >= 0))? 'c' : 'a';
+                estado = ((bolinha.xb < d2) && (vxb >= 0))? 'c' : 'a';
             }
 
             if(estado == 'b')
-            {   
+            {
                 // Regra 2
-                estado = ((xb < d2) && (vxb >= 0))? 'c' : 'b';
+                estado = ((bolinha.xb < d2) && (vxb >= 0))? 'c' : 'b';
             }
 
             if(estado == 'c')
-            {   
+            {
                 // Regra 1
-                estado = ((xb > d1) && (vxb < 0))? 'b' : 'c';
+                estado = ((bolinha.xb > d1) && (vxb < 0))? 'b' : 'c';
                 // Regra 3
-                estado = (xb >= d2)? 'a' : 'c';
+                estado = (bolinha.xb >= d2)? 'a' : 'c';
             }
 
 
@@ -134,20 +137,20 @@ class GenericPlayer {
                     // Goleiro no centro do gol
                     return Utils::Posture(19.00, 65.30, M_PI/4);
                     break;
-                
+
                 case 'b':
                     // Goleiro na posição estimada
                     return Utils::Posture(19.00, y_inters, M_PI/4);
                     break;
-                
+
                 case 'c':
                     // Goleiro seguindo o y da bola
-                    // Dentro dos limites min e max da área                    
-                    yb = (yb > y_min)? (yb < y_max)? yb : y_max : y_min; 
-                    
-                    return Utils::Posture(19.00, yb, M_PI/4);
+                    // Dentro dos limites min e max da área
+                    bolinha.yb = (bolinha.yb > y_min)? (bolinha.yb < y_max)? bolinha.yb : y_max : y_min;
+
+                    return Utils::Posture(19.00, bolinha.yb, M_PI/4);
                     break;
-            
+
                 default:
                     // Goleiro no centro do gol
                     return Utils::Posture(19.00, 65.30, M_PI/4);
@@ -158,10 +161,10 @@ class GenericPlayer {
 
         }
         /**
-         * @brief Calcula valores unitários representando a direção de deslocamento da bola em x. 
-         * 
-         * 
-         * @return int 
+         * @brief Calcula valores unitários representando a direção de deslocamento da bola em x.
+         *
+         *
+         * @return int
          */
         int calcula_variacao_x(float ball_pos, float _ball_pos)
         {
@@ -170,15 +173,15 @@ class GenericPlayer {
             // DEBUGG LINES
             printf("Posição atual da bola: %f \n", ball_pos);
             printf("Posição antiga da bola %f \n", _ball_pos);
-            
+
             return (( ball_pos - _ball_pos) > 0)? 1 : (( ball_pos - _ball_pos) == 0)? 0 : -1;
         }
 
         /**
-         * @brief Calcula valores unitários representando a direção de deslocamento da bola em y. 
-         * 
-         * 
-         * @return int 
+         * @brief Calcula valores unitários representando a direção de deslocamento da bola em y.
+         *
+         *
+         * @return int
          */
         int calcula_variacao_y(float ball_pos, float _ball_pos)
         {
