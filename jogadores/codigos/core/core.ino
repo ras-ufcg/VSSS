1f5c940e8b3b384d17c4aee06955536a3d147020
Lyang Medeiros <lyang.medeiros@ee.ufcg.edu.br>
2019-08-13 19:47:30 -0300

    implementação da captura de interrupções
    o código atual é capaz de retornar o RPM dos dois motores lendo as
    capturando interrupções do sinal gerado pelos encoders magneticos
    dos motores

--- jogadores/codigos/core/core.ino
+++ jogadores/codigos/core/core.ino
@@ -1,42 +1,80 @@
-// Mapeamento de Hardware
+// core.ino - v0.1 - 13/08/2019 - Lyang Leme de Medeiros
+/* =====================================================================
+ * =====================================================================
+ * 
+ * Descrição:
+ * 
+ * Código que roda nos jogadores.
+ * 
+ * =====================================================================
+ * 
+ * Requisitos:
+ * 
+ * Receber e tratar os dados enviados pelo controlador via RF.
+ * Calibrar PWM dos motores ao ligar
+ * Controla a velocidade dos motores do jogador.
+ * 
+ * =====================================================================
+ * 
+ * Detalhes da implementação:
+ * 
+ * Na versão atual o código apenas realiza a captura de interupções
+ * nas portas ligadas aos encoders dos motores para validar tal 
+ * funcionalidade.
+ * 
+ * =====================================================================
+ * 
+ * =====================================================================
+ */
 
-#define A1 16 // D0 do nodemcu ligado ao Pino 2 da ponte H
-#define A2 5  // D1 do nodemcu ligado ao P 7 da ponte
-#define A3 4  // D2 do nodemcu ligado ao P 10 da ponte
-#define A4 0  // D3 do nodemcu ligado ao P 15
+ #define encL    2
+ #define encR    3
 
-#define LED 2 // Led de indicação 
-void setup() {
 
-  pinMode(A1, OUTPUT);
-  pinMode(A2, OUTPUT);
-  pinMode(A3, OUTPUT);
-  pinMode(A4, OUTPUT);
-  pinMode(LED,OUTPUT);
-  
+ void leftPulseCount();
+ void rightPulseCount();
+ 
+ int pulseNumLeft;
+ int pulseNumRight;
+
+ int displayTime = 500;
+ int multiplyFactor = 1000/displayTime;
+ 
+ void setup()
+ {
+    Serial.begin(115200);
+
+    pinMode(encL, INPUT);
+    pinMode(encR, INPUT);
+
+    attachInterrupt(digitalPinToInterrupt(encL), leftPulseCount, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(encR), rightPulseCount, CHANGE);
+ }
+
+void loop()
+{
+    delay(displayTime);
+    
+    Serial.print("RPM roda esquerda: ");
+    Serial.println(pulseNumLeft*multiplyFactor);
+
+    Serial.print("RPM roda direita: ");
+    Serial.println(pulseNumRight*multiplyFactor);
+
+    Serial.print("Diferença: ");
+    Serial.println(abs(pulseNumLeft - pulseNumRight)*multiplyFactor);
+
+   
+    pulseNumLeft = 0;
+    pulseNumRight = 0;
 }
-void loop() {
-  digitalWrite(A1,0);
-  digitalWrite(A2,0);
-  digitalWrite(A3,0);
-  digitalWrite(A4,0);
-  delay(2);
-  digitalWrite(A1,1);
-  digitalWrite(A2,0);
-  digitalWrite(A3,1);
-  digitalWrite(A4,0);
-  digitalWrite(LED,1);
-  delay(1000);
-  digitalWrite(A1,0);
-  digitalWrite(A2,0);
-  digitalWrite(A3,0);
-  digitalWrite(A4,0);
-  delay(2);
-  digitalWrite(A1,0);
-  digitalWrite(A2,1);
-  digitalWrite(A3,0);
-  digitalWrite(A4,1);
-  digitalWrite(LED,0);
-  delay(1000);
-  
+
+void leftPulseCount()
+{
+  pulseNumLeft++; 
+}
+
+void rightPulseCount()
+{
+  pulseNumRight++; 
 }
